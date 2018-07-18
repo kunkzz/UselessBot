9fcca0e25f2b52ce0c0a813f3c4f8cc3701f3f05
kunkzz <37227050+kunkzz@users.noreply.github.com>
2018-07-18 10:13:49 +0100

    Security Update
    Doesnt Display the bot token

--- mybot.js
+++ mybot.js
@@ -1,52 +1,13 @@
+const config = require('./botconfig.json');
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const profanities = ['shit', 'merda', 'fds', 'caralho', 'puta', 'fuck', 'cona', 'pila'];
+const profanities = ['shit', 'merda', 'fds', 'caralho', 'puta', 'fuck', 'cona', 'pila' ,'topkek','caralhete'];
 
-/*Start of Comment
-
-client.on("ready", () => {
-  console.log("I am ready!");
-  for(let i=0;client.guilds.length-1;i++){
-
-
-   for (let j=0;guilds[i].members.length-1;j++){
-        if(guilds[i].members[j].roles.length==0){
-          guilds[i].members[j].addRole('312678877312974852');
-        }
-
-    }
-    console.log("Membros movidos");
-  }
-});
-
-End of Comment*/
-
-
-/*Start of the Comment
-
-client.guilds.forEach((function(guild) {
-  if(guild.name=='Singularity'){
-    guild.members.forEach((function(member){
-      if(member.roles.lenght==0){
-        member.addRole('312678877312974852');
-      }
-    });
-  }
-});
-*/
-
-//Adicionar profanities
-
-/*
 client.on("message", (message) => {
-  if (message.content.startsWith('=')){
-    if (message.content.substring(1,7)=='addprof'){
-      profanities.push(message.content.slice(8,message.lenght-1));
-      console.log(message.content.slice(8,message.lenght-1));
-    }
+  if (message.content.startsWith("yeet")) {
+    message.channel.send("Bitch you thought");
   }
 });
-End of Comment*/
 
 //Swear words
 client.on("message", (message) => {
@@ -60,15 +21,60 @@ client.on("message", (message) => {
   }
 });
 
+
+
 //Join server add Newbs
 client.on('guildMemberAdd', member => {
-member.addRole('312678877312974852');
+  member.addRole('312678877312974852');
 });
 
 
 //Adds Newbs a quem n tenha
 
+//vai buscar o token a config file
+client.login(config.token);
+
+//show the console a Ready message
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+/*  for(let i=0;client.guilds.length-1;i++){
+
+
+for (let j=0;guilds[i].members.length-1;j++){
+      if(guilds[i].members[j].roles.length==0){
+        guilds[i].members[j].addRole('312678877312974852');
+      }
+
+  }
+  console.log("Membros movidos");
+}
 
 
 
-client.login("MzM1MTY1Nzk5NTI4NTI5OTIy.Di-IOQ.XZ44z_uiYSIBmGrCoVjM8cZzgiI");
+
+
+/*Start of the Comment
+
+client.guilds.forEach((function(guild) {
+if(guild.name=='Singularity'){
+  guild.members.forEach((function(member){
+    if(member.roles.lenght==0){
+      member.addRole('312678877312974852');
+    }
+  });
+}
+});
+
+//Adicionar profanities
+
+client.on("message", (message) => {
+if (message.content.startsWith('=')){
+  if (message.content.substring(1,7)=='addprof'){
+    profanities.push(message.content.slice(8,message.lenght-1));
+    console.log(message.content.slice(8,message.lenght-1));
+  }
+}
+});
+End of Comment*/
